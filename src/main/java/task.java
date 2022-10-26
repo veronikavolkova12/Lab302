0ba5a38848c5c24d60755c66646e2a19612f8115
Veronika <veronikacasado@icloud.com>
2022-10-26 18:46:00 +0200

    lab302

---
+++ src/main/java/task.java
@@ -0,0 +1,41 @@
+public Task() {
+        }
+
+public class Task(String title, LocalDate dueDate, boolean status) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.status = status;
+}
+
+public class ong getId() {
+        return id;
+        }
+
+public class void setId(Long id) {
+        this.id = id;
+        }
+
+public class String getTitle() {
+        return title;
+        }
+
+public class void setTitle(String title) {
+        this.title = title;
+        }
+
+public class LocalDate getDueDate() {
+        return dueDate;
+        }
+
+public class void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+        }
+
+public boolean isStatus() {
+        return status;
+        }
+
+public void setStatus(boolean status) {
+        this.status = status;
+        }
+        }
